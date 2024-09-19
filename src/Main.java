1cb5fce525e2b659da5365036de8ac44db46272b
apineiroandion <apineiroandion@danielcastelao.org>
2024-09-19 13:33:58 +0200

    apartado 2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         FileOutputStream fos;
         try {
             fis = new FileInputStream("/home/dam/Desktop/copybytestextos/copybytestexto.txt");
-            fos = new FileOutputStream("/home/dam/Desktop/copybytestextos/copybytestexto2.txt");
+            //al añadir true al constructor completamos el apartado dos del ejercicio, añadiendo al final del archivo
+            //el contenido sin sobreescribirlo
+            fos = new FileOutputStream("/home/dam/Desktop/copybytestextos/copybytestexto2.txt",true);
             int c;
             while ((c = fis.read()) != -1) {
                 fos.write(c);
