bdfe924e48db4cbc14a99d637608cfb36c43dd82
apineiroandion <apineiroandion@danielcastelao.org>
2024-09-19 08:36:43 +0200

    ejercicio terminado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
+        FileInputStream fis;
+        FileOutputStream fos;
         try {
-            fis = new FileInputStream("/home/dam/Desktop/copybytestextos/copybytestextos.txt");
-        }catch (FileNotFoundException e){
-            System.out.println("Error al abrir el archivo");
+            fis = new FileInputStream("/home/dam/Desktop/copybytestextos/copybytestexto.txt");
+            fos = new FileOutputStream("/home/dam/Desktop/copybytestextos/copybytestexto2.txt");
+            int c;
+            while ((c = fis.read()) != -1) {
+                fos.write(c);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
